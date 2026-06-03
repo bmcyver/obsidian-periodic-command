346574cabb52c07cfa51b36695fac3cacb0ca241
bmcyver <bm.cyver@gmail.com>
2026-06-03 17:52:29 +0900

    style: lint

---
+++ eslint.config.mjs
@@ -0,0 +1,24 @@
+import { defineConfig, globalIgnores } from "eslint/config";
+import tseslint from "typescript-eslint";
+import obsidianmd from "eslint-plugin-obsidianmd";
+
+export default defineConfig(
+  obsidianmd.configs.recommended,
+  {
+    files: ["src/**/*.ts"],
+    languageOptions: {
+      parser: tseslint.parser,
+      parserOptions: {
+        project: true,
+      },
+    },
+  },
+  globalIgnores([
+    "dist/**",
+    "node_modules/**",
+    "*.json",
+    "*.mts",
+    "*.mjs",
+    "main.js",
+  ]),
+);
